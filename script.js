4f602d51d639b002022d755275f176f67fbc7efb
Kizbernadet <kizbernadet4@gmail.com>
2024-10-26 22:40:50 +0000

    Mise à jour du site - Ajout du style pour la navbar responsive

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-// variable qui permet de selectionner la div de classe .menu-button
+// Variables qui permettent de sélectionner les éléments
 const menu_button = document.querySelector(".menu-button");
+const menu_mobile = document.querySelector(".navbar");
 
-// cette ligne permet de détecter le clic sur la div burger-button
+// Gestion du clic sur le bouton burger
 menu_button.addEventListener("click", () => {
-    menu_button.classList.toggle("active")
-} );
+    // Ajout/Retrait de la classe 'active' pour le bouton burger
+    menu_button.classList.toggle("active");
+
+    // Ajout/Retrait de la classe 'active' pour le menu mobile
+    menu_mobile.classList.toggle("active");
+});
